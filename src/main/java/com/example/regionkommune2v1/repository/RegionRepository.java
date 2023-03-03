05e266f1878ed341311c1c08066c9e7079c6ef4e
MalikKtK <mali0237@stud.kea.dk>
2023-03-03 09:58:33 +0100

    Initial commit

---
+++ src/main/java/com/example/regionkommune2v1/repository/RegionRepository.java
@@ -0,0 +1,8 @@
+package com.example.regionkommune2v1.repository;
+
+import com.example.regionkommune2v1.model.Region;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RegionRepository extends JpaRepository<Region, String> {
+
+}
